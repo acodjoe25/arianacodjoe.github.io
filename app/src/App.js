31d0eb314ca81ea3fb28b12413d48b8275cbe683
Ariana Codjoe <ariana_codjoe@brown.edu>
2024-03-11 16:05:15 +0100

    setup

---
+++ app/src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+
+import React from 'react';
+/**
+ * This is the highest level component!
+ */
+function App() {
+    return (
+      <div className="App">
+        <p className="App-header">
+          <h1>hello its a me</h1>
+          
+        </p>
+      </div>
+    );
+  }
+  
+  export default App;
---
+++ app/src/header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+/**
+ * This is the header 
+ */
+function Header() {
+    return (
+      <div className="App">
+        <p className="App-header">
+          <h1>hello its a me</h1>
+        </p>
+      </div>
+    );
+  }
+  
+  export default Header;
