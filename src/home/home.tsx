6f84ed9a4f054b4ef1ec95bcfe0b44f917431b38
Ariana Codjoe <ariana_codjoe@brown.edu>
2024-03-13 19:10:40 +0100

    home page = done

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -20,14 +20,16 @@ const Home: React.FC = () => {
                     className='myImage'
                     alt='an image of me from my linkedin'>
                 </img>
-                {/* <a className='git' href="https://github.com/acodjoe25">See my GitHub!</a>
-                <a className='link' href="https://www.linkedin.com/in/ariana-codjoe-771603230/">Connect one Linkedin!</a> */}
+                {/* <div className='socials'> */}
+                    <a href="https://github.com/acodjoe25" className="git">GitHub</a>
+                    <a className='link' href="https://www.linkedin.com/in/ariana-codjoe-771603230/">Linkedin</a>
+                {/* </div> */}
             </div>
             <div className='bioText'>
                 <p>
                     As a junior at Brown University majoring in Computer Science and Applied Mathematics, I'm deeply intrigued by the analytical nature of mathematics and the creative expression found in theater. I see parallels between structured problem-solving in mathematics and individual interpretation in theater, which led me to pursue computer science. I'm fascinated by the blend of structure and creativity in software engineering, likening it to crafting characters in a play.
         Moreover, I'm drawn to exploring storytelling through computer science, particularly in mediums like virtual reality. I'm also passionate about the ethical considerations in computer science and actively involved in diversity-related efforts within the field. I relish the collaborative aspect of software engineering, particularly in daily standup sessions where teamwork and knowledge sharing thrive. Witnessing my contributions seamlessly integrate into larger projects is deeply fulfilling, reminiscent of my experiences in high school crew.
-        Overall, I find software engineering's dynamic nature, collective accomplishments, and opportunities for personal growth and creativity a perfect fit for my diverse interests and strengths.
+        Overall, I find software engineering's dynamic nature, collective accomplishments, and opportunities for personal growth and creativity a perfect fit for my diverse interests and strengths. Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello Hello
                 </p>
             </div>
         </div>
