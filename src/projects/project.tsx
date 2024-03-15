9b2bfd3309992ca68cdb308ccf62ba481ae06707
Ariana Codjoe <ariana_codjoe@brown.edu>
2024-03-15 09:38:28 +0100

    projects page done

--- src/projects/project.tsx
+++ src/projects/project.tsx
@@ -8,9 +8,37 @@ import React, { useState } from 'react';
 const Project: React.FC = () => {
 
     return (
-        <p>
-            Hello its the projects
-        </p>
+        <div className='tabs'>
+            <img className='dataServer' 
+            src='https://i.postimg.cc/sXT535tk/dataserver.png'
+            alt='data server explanation (see github readme)'></img>
+
+            <img className='ghiblify' 
+            src='https://i.postimg.cc/RZPRS5Ks/ghiblify.png'
+            alt='data server explanation (see github readme)'></img>
+
+            <img className='shell' 
+            src='https://i.postimg.cc/SQWfXzcz/shell.png'
+            alt='shell explanation (see github readme)'></img>
+
+            <img className='malloc' 
+            src='https://i.postimg.cc/mkRM3871/malloc.png'
+            alt='malloc explanation (see github readme)'></img>
+
+            <img className='decisionTree' 
+            src='https://i.postimg.cc/jqgd4M6V/decision-Tree.png'
+            alt='decision tree explanation (see github readme)'></img>
+
+            <img className='search' 
+            src='https://i.postimg.cc/HkQgRzMk/search.png'
+            alt='searchexplanation (see github readme)'></img>
+
+            <img className='website' 
+            src='https://i.postimg.cc/LsQ23Xft/website.png'
+            alt='this website!'></img>
+
+
+        </div>
     );
   }
   
