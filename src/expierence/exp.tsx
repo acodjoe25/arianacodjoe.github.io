85ac7abd1eec3ff0677e55233fd26c2870ed46d7
Ariana Codjoe <ariana_codjoe@brown.edu>
2024-04-02 10:17:24 +0200

    final section

--- src/expierence/exp.tsx
+++ src/expierence/exp.tsx
@@ -8,10 +8,13 @@ import React, { useState } from 'react';
 const Exp: React.FC = () => {
 
     return (
-        <p>
-            Hello its the expierence
-        </p>
-    );
+        <div className='photos'>
+            <img className='brown' 
+            src='https://i.postimg.cc/dVJWgV81/Screenshot-2024-04-02-at-10-11-36-AM.png' 
+            // TODO upload image
+            alt='expierence learning @ brown + working there'></img>
+        </div>
+    )
   }
   
   export default Exp;
