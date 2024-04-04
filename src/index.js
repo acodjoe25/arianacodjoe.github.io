6f0ce75a1a0a3b22312ad022744029159db01e58
Ariana Codjoe <ariana_codjoe@brown.edu>
2024-04-04 13:54:37 +0200

    trying what online ppl say

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import reportWebVitals from './reportWebVitals';
+import React, { useState } from "https://cdn.skypack.dev/react";
+import ReactDOM from "https://cdn.skypack.dev/react-dom";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
