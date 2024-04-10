923b70942f376117080b91ba5199ef71c55b9a7c
Ariana Codjoe <ariana_codjoe@brown.edu>
2024-04-10 14:13:17 +0200

    18

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 // import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals.js';
 import React, { useState } from "https://cdn.skypack.dev/react";
 import ReactDOM from "https://cdn.skypack.dev/react-dom";
 
